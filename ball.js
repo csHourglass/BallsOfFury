ce48277a96f70796c1710e262c1e8df4a488ee5c
Alvin Nguyen <alvin@D-10-16-4-27.dhcp4.washington.edu>
2018-01-31 16:41:36 -0800

    fixed dcharge throw bouncing

--- ball.js
+++ ball.js
@@ -16,7 +16,6 @@ function Ball(game, x, y, team, chargingTime) {
     this.width = 20;
     this.team = team;
 
-    this.speed = 1500;
     this.state = 0;
     this.targetx = game.mousex - x - 50;
     this.targety = game.mousey - y - 50;
@@ -31,12 +30,14 @@ function Ball(game, x, y, team, chargingTime) {
 		chargingTime = 3;
 	}
 	this.chargingTime = chargingTime;
+    //max charge time will double the speed of the ball.
+    this.speedDefault = 1500;
+    this.speed = this.speedDefault + (this.speedDefault * chargingTime/3);
+    //how much the speed will decrease by 
+    this.speedDecrease = 100;
+
     this.ySpeed = this.targety / (Math.sqrt(Math.pow(this.targetx, 2) + Math.pow(this.targety, 2)));
-	//arbitrary calculation for how much charging affects yspeed
-	this.ySpeed += (this.ySpeed * (chargingTime/2));
     this.xSpeed = this.targetx / (Math.sqrt(Math.pow(this.targetx, 2) + Math.pow(this.targety, 2)));
-	//arbitrary calculation for how much charging affects xspeed
-	this.xSpeed += (this.xSpeed * (chargingTime/2));
 
     Entity.call(this, game, this.x, this.y, 0, 0, true);
 
@@ -46,6 +47,12 @@ Ball.prototype = new Entity();
 Ball.prototype.constructor = Ball;
 
 Ball.prototype.update = function() {
+    if (this.speed > 1500) {
+        this.speedDecrease = 400;
+    }
+    else {
+        this.speedDecrease = 100;
+    }
 //console.log(this.team);
     if (this.state === 0)   {
         this.x += this.game.clockTick * this.speed * this.xSpeed;
@@ -53,11 +60,11 @@ Ball.prototype.update = function() {
 
         if (this.x > width || this.x < 0) {
             this.xSpeed = -this.xSpeed;
-            this.speed -= 100;
+            this.speed -= this.speedDecrease;
         }
         if (this.y > height - 64 || this.y < 0)  {
             this.ySpeed = -this.ySpeed;
-            this.speed -= 100;
+            this.speed -= this.speedDecrease;
         }
         if (this.speed < 1500)   {
             this.state++;
@@ -72,7 +79,7 @@ Ball.prototype.update = function() {
         }
         if (this.y > height - 32)   { //If the ball hits the ground...
             this.y = height - 32;
-            this.speed -= 100; //Lower speed overall
+            this.speed -= this.speedDecrease; //Lower speed overall
             this.ySpeed = -(this.ySpeed/1.5); //Reverse ySpeed on bounce and reduce magnitude.
             if (this.speed <= 0)   { //Once speed is completely depleted, enter state 2
                 this.state = 2;
