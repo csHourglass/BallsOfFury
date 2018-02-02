1ef0e3d311845ac4c15b67e679bd2f3bcbe7df97
alveeno <nguyenalvind@gmail.com>
2018-02-01 23:43:08 -0800

    ...the whole time i forgot to send the files. that is why we didnt have access to the files. sigh

--- app.js
+++ app.js
@@ -7,8 +7,19 @@ var io = require('socket.io').listen(server);
 server.listen(3000);
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/animation.js');
+  res.sendFile(__dirname + '/assetmanager.js');
+  res.sendFile(__dirname + '/ball.js');
+  res.sendFile(__dirname + '/dummy.js');
+  res.sendFile(__dirname + '/gameengine.js');
+  res.sendFile(__dirname + '/main.js');
+  res.sendFile(__dirname + '/player.js');
 });
 
 io.on('connection', function(socket){
   console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
+
