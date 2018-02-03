6325220e5b8d959bc949b4ffc0b2ffa732fdc596
Alvin Nguyen <alvin@D-10-16-4-27.dhcp4.washington.edu>
2018-02-02 18:09:59 -0800

    test

--- main.js
+++ main.js
@@ -24,6 +24,18 @@ Background.prototype.draw = function (ctx) {
 var ASSET_MANAGER = new AssetManager();
 
 
+
+ASSET_MANAGER.queueDownload("./img/Stage1Background.png");
+ASSET_MANAGER.queueDownload("./img/TestPlatform.png");
+ASSET_MANAGER.queueDownload("./img/TestWall.png");
+ASSET_MANAGER.queueDownload("./img/player.png");
+
+
+
+
+
+
+
 //ASSET_MANAGER.queueDownload("./img/bg15.png");
 ASSET_MANAGER.queueDownload("./img/bg20.png");
 ASSET_MANAGER.queueDownload("./img/Stage1Background.png");
