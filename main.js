f75f2cec4793b68af8cfec8a82b3680c5465747e
Hour <csHourglass@gmail.com>
2018-02-06 13:03:29 -0800

    Edited Background to allow any image size.
    Background now automatically scales images to fill the canvas.

--- main.js
+++ main.js
@@ -2,9 +2,15 @@ var width = 1920;
 var height = 1080;
 
 
-function Background(game) {
+function Background(game, bg) {
 //    this.bgAnimation = new Animation(ASSET_MANAGER.getAsset("./img/bg15.png"), 0, 0, width, height, 0.1, 8, true, false);
-    this.bgAnimation = new Animation(ASSET_MANAGER.getAsset("./img/Stage1Background.png"), 0, 0, width, height, 1, 1, true, false);
+    var xscale = width/bg.width;
+    var yscale = height/bg.height;
+
+    if (xscale > yscale) this.scale = xscale;
+    else this.scale = yscale;
+    
+    this.bgAnimation = new Animation(bg, 0, 0, bg.width, bg.height, 1, 1, true, false);
 
 
     Entity.call(this, game, 0, 0, 0, 0, false);
@@ -16,7 +22,7 @@ Background.prototype.update = function () {
 }
 
 Background.prototype.draw = function (ctx) {
-    this.bgAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y);
+    this.bgAnimation.drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
 
 }
 //////////////// the "main" code begins here  ////////////////////////
@@ -27,6 +33,7 @@ var ASSET_MANAGER = new AssetManager();
 //ASSET_MANAGER.queueDownload("./img/bg15.png");
 ASSET_MANAGER.queueDownload("./img/bg20.png");
 ASSET_MANAGER.queueDownload("./img/Stage1Background.png");
+ASSET_MANAGER.queueDownload("./img/bg1.jpg");
 ASSET_MANAGER.queueDownload("./img/TestPlatform.png");
 ASSET_MANAGER.queueDownload("./img/TestWall.png");
 ASSET_MANAGER.queueDownload("./img/TestCeiling.png");
@@ -52,7 +59,7 @@ ASSET_MANAGER.downloadAll(function () {
     //var player1 = new Player(gameEngine, 128, 700, 1, ASSET_MANAGER.getAsset("./img/player.png"));
     //var player2 = new Player (gameEngine, 800, 400, 2, ASSET_MANAGER.getAsset("./img/player.png"));
 
-    gameEngine.addEntity(new Background(gameEngine, ASSET_MANAGER.getAsset("./img/Stage1Background.png")));
+    gameEngine.addEntity(new Background(gameEngine, ASSET_MANAGER.getAsset("./img/bg1.jpg")));
     gameEngine.addEntity(new Wall(gameEngine, 0, 918, 1920, 162, "./img/TestPlatform.png", 0, ASSET_MANAGER.getAsset("./img/TestPlatform.png")));
     gameEngine.addEntity(new Wall(gameEngine, 21, 72, 48, 846, "./img/TestWall.png", 0, ASSET_MANAGER.getAsset("./img/TestWall.png")));
     gameEngine.addEntity(new Wall(gameEngine, 1851, 72, 48, 846, "./img/TestWall.png", 0, ASSET_MANAGER.getAsset("./img/TestWall.png")));
