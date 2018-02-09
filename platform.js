1e9f7a480ee2767d9c4f479312d5bf3500455482
Hour <csHourglass@gmail.com>
2018-02-09 13:19:41 -0800

    Platform implemented.
    Jumping onto platforms works as intended.  Platforms only have collision on the top.  The platform constructor also has a new parameter: scale.  This scales the image down so we can use large images for small entities.

--- platform.js
+++ platform.js
@@ -1,22 +1,23 @@
 
 
-function Platform(game, x, y, width, height, path, team)   {
+function Platform(game, x, y, width, height, path, team, scale)   {
     //Loading Animations
 //// Right Animations ////
     this.anim = new Animation(ASSET_MANAGER.getAsset(path), 0, 0, width, height, 1, 1, true, false);
 
     this.x = x;
     this.y = y;
-    this.width = width;
-    this.height = height;
-    this.boundingBox = new BoundingBox(this.x, this.y, this.width, this.height);
+    this.width = width * scale;
+    this.height = height * scale;
+    this.scale = scale;
+    this.boundingBox = new BoundingBox(this.x, this.y, this.width, 0);
     this.showBoxes = true;  // show Bounding boxes for testing
     this.team = team;
 
     Entity.call(this, game, this.x, this.y, 0, 0, true, 2);
 }
 Platform.prototype = new Entity();
-Platform.prototype.constructor = Wall;
+Platform.prototype.constructor = Platform;
 
 
 Platform.prototype.update = function ()   {
@@ -40,7 +41,7 @@ Platform.prototype.draw = function(ctx)   {
         this.boundingBox.draw(ctx);
     }
 
-    this.anim.drawFrame(0, ctx, this.getX(), this.getY(), this.game.drawScale);
+    this.anim.drawFrame(0, ctx, this.getX(), this.getY(), this.game.drawScale*this.scale);
 
     Entity.prototype.draw.call(this);
 }
